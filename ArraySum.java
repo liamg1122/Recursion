946c5c769561cdf6e96822188dd5a5b54f2c6aa8
liamg1122 <75380265+liamg1122@users.noreply.github.com>
2024-02-20 22:02:02 -0500

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,18 @@
+
+public class ArraySum {
+	  
+	
+	public int sumOfArray(Integer[] a, int index) {
+	        
+		if (index < 0) {
+	            
+			return 0;
+	        
+		} else {
+	            
+			return a[index] + sumOfArray(a, index - 1);
+	        
+		}
+	    
+	}
+}
